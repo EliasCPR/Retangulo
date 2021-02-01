3f317cb3994bcc83ae383adeca97f7ddb5404041
ELIAS MATEUS PRADO OLIVEIRA <20272513@cfp127aluno.local>
2021-02-01 11:30:49 -0300

    atualização do retangulo

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/67/507399307b64001b12d0e9902b398260
@@ -0,0 +1,35 @@
+package br.com.empresa.gui;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class FrameRetangulo {
+	
+	public void criarTela() {
+		
+		JFrame tela = new JFrame();
+		tela.setTitle("Calculos com retângulo");
+		
+		tela.setSize(600, 400);
+		tela.setDefaultCloseOperation(3);
+		tela.setLayout(null);
+		
+		
+//		construir um JLabel
+		
+		JLabel labelAltura = new JLabel();
+		labelAltura.setText("Altura: ");
+		tela.getContentPane().add(labelAltura);
+		labelAltura.setBounds(10, 10, 60, 30);
+		
+		JTextField textAltura = new JTextField();
+		textAltura.setBounds(70, 10, 50, 30);
+		tela.getContentPane().add(textAltura);
+
+		
+		tela.setVisible(true);
+		
+	}
+	
+}
--- retangulo/src/br/com/empresa/gui/FrameRetangulo.java
+++ retangulo/src/br/com/empresa/gui/FrameRetangulo.java
@@ -1,8 +1,17 @@
 package br.com.empresa.gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+
+import br.com.empresa.model.Retangulo;
 
 public class FrameRetangulo {
 	
@@ -16,20 +25,85 @@ public class FrameRetangulo {
 		tela.setLayout(null);
 		
 		
-//		construir um JLabel
+//		build one JLabel
 		
 		JLabel labelAltura = new JLabel();
 		labelAltura.setText("Altura: ");
-		tela.getContentPane().add(labelAltura);
 		labelAltura.setBounds(10, 10, 60, 30);
+		
+//		base JLabel  
+		
+		JLabel labelBase = new JLabel();
+		labelBase.setText("Base:");
+		labelBase.setBounds(10, 50, 60, 30);
+		
+//		build one textField
 		
 		JTextField textAltura = new JTextField();
 		textAltura.setBounds(70, 10, 50, 30);
+		
+//      Base textField  
+		
+		JTextField textBase = new JTextField();
+		textBase.setBounds(70, 50, 50, 30);
+		
+//		construir um botao
+		
+		JButton btnCalcular = new JButton();
+		btnCalcular.setText("calcular");
+		btnCalcular.setBounds(10, 100, 110, 30);
+		
+		JButton btnLimpar = new JButton();
+		btnLimpar.setText("limpar");
+		btnLimpar.setBounds(10,140, 110, 30);
+		
+//		jlabel do perimetro e da area
+		
+		JLabel labelArea = new JLabel();
+		labelArea.setText("Perímetro:");
+		labelArea.setBounds(150, 10, 70, 30);
+		
+		JLabel labelPerimetro = new JLabel();
+		labelPerimetro.setText("Perímetro:");
+		labelPerimetro.setBounds(150, 50, 70, 30);
+		
+//		JLabel de valores 
+		
+		JLabel labelValorArea = new JLabel();
+		
 		tela.getContentPane().add(textAltura);
-
+		tela.getContentPane().add(labelAltura);
+		tela.getContentPane().add(labelAltura);
+		tela.getContentPane().add(textBase);
+		tela.getContentPane().add(labelBase);
+		tela.getContentPane().add(labelBase);
+		tela.getContentPane().add(btnCalcular);
+		tela.getContentPane().add(btnLimpar);
+		tela.getContentPane().add(labelArea);
+		tela.getContentPane().add(labelPerimetro);
 		
 		tela.setVisible(true);
 		
+//		ouvintes de ações /eventos
+		
+		btnCalcular.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+				Retangulo r1 = new Retangulo();
+				r1.setAltura(Double.parseDouble(textAltura.getText()));
+				r1.setBase(Double.parseDouble(textBase.getText()));
+				
+				labelArea.setText(String.valueOf(r1.calcularArea()));
+				labelPerimetro.setText(String.valueOf(r1.calcularPerimetro()));
+			}
+			
+			
+		});
+		
+				
 	}
 	
 }
